4a1a8525344d1a76cafb4e4be87b5062e61fb89d
shiichi <s.shiichi311041@gmail.com>
2015-12-18 16:10:27 +0900

    remove todomvc

--- src/js/constants/ActionTypes.js
+++ src/js/constants/ActionTypes.js
@@ -1,15 +1,3 @@
-//TodoApp
-export const ADD_TODO = 'ADD_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
-export const EDIT_TODO = 'EDIT_TODO';
-export const COMPLETE_TODO = 'COMPLETE_TODO';
-export const COMPLETE_ALL = 'COMPLETE_ALL';
-export const CLEAR_COMPLETED = 'CLEAR_COMPLETED';
-
-/*
-Drone system
-*/
-
 //request user infomation
 export const REQUEST_USERINFO = 'REQUEST_USERINFO';
 export const REQUEST_USERINFO_SUCCESS = 'REQUEST_USERINFO_SUCCESS';
