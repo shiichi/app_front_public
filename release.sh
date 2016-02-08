52d09e70d081c64f9e75f7f31733cb0c6b21a2c6
atyenoria <eigotyoubunn30@gmail.com>
2016-02-08 19:46:27 +0900

    test

--- release.sh
+++ release.sh
@@ -4,15 +4,18 @@
 
 set -e
 
+    LARABEL_REPO: test
+    LARABEL_BRANCH: sample
+
 git config --global user.email "eigotyoubunn30@gmail.com" #コミットユーザの有効なメールアドレス
 git config --global user.name "atyenoria" #コミットユーザの名前
 
 # リリースバージョン名
 CI_RELEASE_VERSION=`date +"v%Y%m%d%H%M%S"`
 # リリースブランチ名
-CI_RELEASE_BRANCH="release/${CI_RELEASE_VERSION}"
+CI_RELEASE_BRANCH="${LARABEL_BRANCH}"
 # リモートリポジトリ
-CI_REMOTE_REPOSITORY="git@github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git"
+CI_REMOTE_REPOSITORY="git@github.com:${CIRCLE_PROJECT_USERNAME}/${LARABEL_REPO}.git"
 # リリースブランチ作成
 git checkout -b ${CI_RELEASE_BRANCH}
 #変更ファイルををすべて追加
