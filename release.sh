75e8f3e2ad82027865307289b9fcf1ecbaee5db1
atyenoria <eigotyoubunn30@gmail.com>
2016-02-08 20:10:53 +0900

    build

--- release.sh
+++ release.sh
@@ -1,23 +1,14 @@
-
-#!/bin/bash
-# gitユーザ設定
+#!/bin/sh
 
 set -e
 
-git config --global user.email "eigotyoubunn30@gmail.com" #コミットユーザの有効なメールアドレス
-git config --global user.name "atyenoria" #コミットユーザの名前
+git config --global user.email "eigotyoubunn30@gmail.com"
+git config --global user.name "atyenoria"
 
-# リリースバージョン名
 CI_RELEASE_VERSION=`date +"v%Y%m%d%H%M%S"`
-# リリースブランチ名
 CI_RELEASE_BRANCH="${LARABEL_BRANCH}"
-# リモートリポジトリ
 CI_REMOTE_REPOSITORY="${LARABEL_REPO}"
-# リリースブランチ作成
 git checkout -b ${CI_RELEASE_BRANCH}
-#変更ファイルををすべて追加
 git add --all :/
-#コミット
 git commit -m "[auto] release branch (${CI_RELEASE_VERSION})"
-#リモートにプッシュ
 git push ${CI_REMOTE_REPOSITORY} ${CI_RELEASE_BRANCH}
