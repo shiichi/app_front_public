fda0ef0513f822b8b45b4f3b64c46f39f1043f36
atyenoria <eigotyoubunn30@gmail.com>
2016-02-08 20:39:11 +0900

    build

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ set -e
 git config --global user.email "eigotyoubunn30@gmail.com"
 git config --global user.name "atyenoria"
 
-CI_RELEASE_VERSION=`date +"v%Y%m%d%H%M%S"`
+CI_RELEASE_VERSION=`date +"v%Y/%m/%d %H:%M:%S"`
 CI_RELEASE_BRANCH="${LARABEL_BRANCH}"
 CI_REMOTE_REPOSITORY="${LARABEL_REPO}"
 git checkout -b ${CI_RELEASE_BRANCH}
